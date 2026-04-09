93e0a235e9aa6ceb10e45710c9c80b25802a5509
afrutos <afrutos@frba.utn.edu.ar>
2026-04-09 20:08:42 +0000

    Se realiza etapa 1, 2 y 3 de tp0

--- client/src/client.c
+++ client/src/client.c
@@ -1,5 +1,5 @@
 #include "client.h"
-
+#include "readline/readline.h"
 int main(void)
 {
 	/*---------------------------------------------------PARTE 2-------------------------------------------------------------*/
@@ -18,7 +18,7 @@ int main(void)
 
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
-
+	log_info(logger,"Hola! Soy un log");
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
@@ -27,8 +27,14 @@ int main(void)
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
 
-	// Loggeamos el valor de config
+	ip = config_get_string_value(config,"IP");
+	puerto = config_get_string_value(config,"PUERTO");
+	valor = config_get_string_value(config,"CLAVE");
 
+	// Loggeamos el valor de config
+	log_info(logger,valor);
+	log_info(logger,puerto);
+	log_info(logger,ip);
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
@@ -55,14 +61,14 @@ int main(void)
 t_log* iniciar_logger(void)
 {
 	t_log* nuevo_logger;
-
+    nuevo_logger = log_create("tp0.log","client.c",1,LOG_LEVEL_INFO);
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
 	t_config* nuevo_config;
-
+	nuevo_config = config_create("cliente.config");
 	return nuevo_config;
 }
 
@@ -74,10 +80,13 @@ void leer_consola(t_log* logger)
 	leido = readline("> ");
 
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
-
+	while(strcmp(leido,"") != 0) {
+		log_info(logger,leido);
+		leido = readline("> ");
+	}
 
 	// ¡No te olvides de liberar las lineas antes de regresar!
-
+	free(leido);
 }
 
 void paquete(int conexion)
@@ -97,4 +106,6 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	  log_destroy(logger);
+	  config_destroy(config);
 }
